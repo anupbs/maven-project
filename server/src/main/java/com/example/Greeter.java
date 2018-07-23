289be212acb077e573eaad317e7bc666f4dd12cb
anup.bilchod <anup.stormy@gmail.com>
2018-07-23 15:36:05 +0100

    fix checkstyle 2

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,10 +12,10 @@ public class Greeter {
 
   }
 
-  /*@param some name
+  /**
+  *@param some name
   *@return greeting string
   */
-  
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
